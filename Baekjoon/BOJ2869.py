b46f1733c99943233c5d09bf1a7fb085e5b1a152
GukJang <gukjang1997@gmail.com>
2023-07-25 05:59:00 +0900

    round 함수 대체
    왜 안되는지 모르겠다

--- Baekjoon/BOJ2869.py
+++ Baekjoon/BOJ2869.py
@@ -1,8 +1,20 @@
 def goUp(up, down, height):
-    day = 1
+    '''day = 1
     while (up*day - down*(day-1) < height):
         day += 1
-    print(day)
+    print(day)'''
+    speed = up - down
+    day = ((height - up)/speed)
+
+    if(day - int(day)) >= 0.5:
+        day = int(day) + 1
+    else:
+        day = int(day)
+
+    if(day < 1):
+        print(day+2)
+    else:
+        print(day+1)
 
 A, B, C = map(int, input().split())
 goUp(A, B, C)
