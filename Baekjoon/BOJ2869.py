449eea8111e4e7812d971cb58f8c40b82ac68968
GukJang <gukjang1997@gmail.com>
2023-07-25 15:42:53 +0900

    반올림x / 올림 math.ceil() 로

--- Baekjoon/BOJ2869.py
+++ Baekjoon/BOJ2869.py
@@ -1,3 +1,4 @@
+import math
 def goUp(up, down, height):
     '''day = 1
     while (up*day - down*(day-1) < height):
@@ -5,16 +6,7 @@ def goUp(up, down, height):
     print(day)'''
     speed = up - down
     day = ((height - up)/speed)
-
-    if(day - int(day)) >= 0.5:
-        day = int(day) + 1
-    else:
-        day = int(day)
-
-    if(day < 1):
-        print(day+2)
-    else:
-        print(day+1)
+    print(math.ceil(day)+1)
 
 A, B, C = map(int, input().split())
 goUp(A, B, C)
