a1f6037bf934c33d88996787bee5a94e85638371
GukJang <gukjang1997@gmail.com>
2023-07-26 18:18:37 +0900

    알파벳 찾기
    find 함수 공부

---
+++ Baekjoon/BOJ10809.py
@@ -0,0 +1,16 @@
+S = str(input())
+
+def alphabetOrder(n):
+    input = list(n)
+    arr = [-1] * 26
+
+    for i in range(len(input)):
+        if(arr[ord(input[i])-97] == -1):
+            arr[ord(input[i])-97] = i
+
+    for i in range(len(arr)):
+        print(str(arr[i]), end=" ")
+
+alphabetOrder(S)
+
+# find 함수 이용하면 훨씬 간단함
