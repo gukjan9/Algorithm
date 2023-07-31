639b4350d4cf432aa914a657980488d131295604
GukJang <gukjang1997@gmail.com>
2023-07-31 23:26:35 +0900

    deque 풀이

--- Baekjoon/BOJ2164.py
+++ Baekjoon/BOJ2164.py
@@ -1,22 +1,35 @@
 # 2164. 카드2
+from collections import deque
+
 def card2(N):
-    queue = []
-    for i in range(N):
-        queue.append(i+1)
-    N_List = queue
+    # queue = []
+    # for i in range(N):
+    #     queue.append(i+1)
+    # N_List = queue
+    #
+    # for i in range(len(N_List)):
+    #     if(len(N_List) < 2):
+    #         return N_List
+    #     else:
+    #         N_List.pop(0)
+    #         if(len(N_List) < 2):
+    #             return N_List
+    #         else:
+    #             a = N_List.pop(0)
+    #             N_List.append(a)
+    #
+    # return N_List
 
-    for i in range(len(N_List)):
-        if(len(N_List) < 2):
-            return N_List
-        else:
-            N_List.pop(0)
-            if(len(N_List) < 2):
-                return N_List
-            else:
-                a = N_List.pop(0)
-                N_List.append(a)
+    deq = deque([i for i in range(1, N+1)])
+    while len(deq) > 1:
+        deq.popleft()
+        deq.append(deq.popleft())
 
-    return N_List
+    return deq.pop()
 
 N = int(input())
-print(*card2(N))
+# print(*card2(N))
+print(card2(N))
+
+# https://wiki.python.org/moin/TimeComplexity
+# https://docs.python.org/ko/3/library/collections.html#collections.deque
