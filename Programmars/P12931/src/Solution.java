9545b4bf95f58585e0d6d1b4b4f2033a85d26141
GukJang <9ukjang@gmail.com>
2023-08-30 17:17:12 +0900

    자릿수 더하기

---
+++ Programmars/P12931/src/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public static int solution(int n) {
+        int answer = 0;
+        String[] str = String.valueOf(n).split("");
+
+        for(int i = 0; i < str.length; i++){
+            answer +=  Integer.parseInt(str[i]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(456));
+    }
+}
