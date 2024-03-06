09fa9f9c527b02a6bc207948126499c515078335
GukJang <9ukjang@gmail.com>
2024-03-06 22:42:13 +0900

    Number Format 에러

---
+++ Baekjoon/Bronze5/BOJ10998/src/BOJ10998.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10998{
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(bf.readLine());
+        int B = Integer.parseInt(bf.readLine());
+
+        System.out.println(A*B);
+    }
+}
