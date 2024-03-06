ff3c0131066b08e454272185e82326bfa9032898
GukJang <9ukjang@gmail.com>
2024-03-06 22:45:11 +0900

    AxB

--- Baekjoon/Bronze5/BOJ10998/src/BOJ10998.java
+++ Baekjoon/Bronze5/BOJ10998/src/BOJ10998.java
@@ -1,12 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class BOJ10998{
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int A = Integer.parseInt(bf.readLine());
-        int B = Integer.parseInt(bf.readLine());
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
 
         System.out.println(A*B);
     }
