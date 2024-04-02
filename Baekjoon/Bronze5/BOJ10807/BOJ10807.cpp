84f3961513c4a55fa1837580c154308e1cd4ce93
GukJang <9ukjang@gmail.com>
2024-04-02 14:51:01 +0900

    개수 세기

---
+++ Baekjoon/Bronze5/BOJ10807/BOJ10807.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N, v, cnt = 0;
+    cin >> N;
+
+    int arr[N];
+
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+
+    cin >> v;
+
+    for(int i = 0; i < N; i++){
+        if(arr[i] == v){
+            cnt++;
+        }
+    }
+
+    cout << cnt << "\n";
+}
