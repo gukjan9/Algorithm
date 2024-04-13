77e3660149d5655b79bab02b9d1481c7d41e87e5
GukJang <9ukjang@gmail.com>
2024-04-13 15:43:39 +0900

    [Solved / C++] 단어의 개수

---
+++ Baekjoon/Bronze2/BOJ1152/BOJ1152.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int cnt = 0;
+    string s;
+    getline(cin, s);
+
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == ' ') cnt++;
+    }
+
+    if(s[0] == ' ') cnt--;
+    if(s[s.length() - 1] == ' ') cnt--;
+
+    cout << cnt+1 << "\n";
+}
