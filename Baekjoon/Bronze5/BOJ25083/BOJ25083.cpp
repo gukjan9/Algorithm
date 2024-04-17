17ac739c52edf97f5058e5fa51cadc5362d613de
GukJang <9ukjang@gmail.com>
2024-04-17 12:01:27 +0900

    [Solved / C++] 새싹
    - 맞힌 사람들은 puts 를 주로 쓴다

---
+++ Baekjoon/Bronze5/BOJ25083/BOJ25083.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+}
